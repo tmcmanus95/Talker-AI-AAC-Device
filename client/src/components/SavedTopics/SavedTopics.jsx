1ccd0acb8f6a7c6c48c89c598dde01a9cbcb897d
tmcmanus95 <tmcmanus95@gmail.com>
2024-01-02 20:02:28 -0600

    added savedTopics component, profile page

---
+++ client/src/components/SavedTopics/SavedTopics.jsx
@@ -0,0 +1,28 @@
+import "./SavedTopics.scss";
+export default function SavedTopics({ username, topics }) {
+  return (
+    <section>
+      <h1>{username}'s Saved Topics</h1>
+      <div>
+        <div>
+          {topics &&
+            topics.map((topic) => (
+              <div key={topic.topic._id}>
+                <div>
+                  <h4>{topic.topic.promptText}</h4>
+                  <ul>
+                    {topic.topic.responses.map((response) => (
+                      <li key={response._id}>
+                        <p>{response.responseText}</p>
+                        {response.imageURL && <img src={response.imageURL} />}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
+}
