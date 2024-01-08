cda673ae5f64ebb115f9925de722835bc9a018fb
tmcmanus95 <tmcmanus95@gmail.com>
2024-01-07 21:40:42 -0600

    reconfigured .env

--- client/src/components/RecordButton/RecordButton.jsx
+++ client/src/components/RecordButton/RecordButton.jsx
@@ -1,76 +1,66 @@
-import { useState, useEffect } from "react";
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import { PromptTemplate } from "langchain/prompts";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
-import { QUERY_ME } from "../../../utils/queries";
 import { createClient } from "pexels";
+import axios from "axios";
 import BigResponse from "../BigResponseComponent/BigResponse";
-import ResponseForm from "../ResponseForm/ResponseForm";
-import TopicForm from "../TopicForm/TopicForm";
 import "./RecordButton.scss";
+import { QUERY_ME } from "../../../utils/queries";
 
 export default function RecordButton() {
   const [userInput, setUserInput] = useState("");
   const [responses, setResponses] = useState([]);
   const [promptText, setPromptText] = useState("");
   const [imageURLs, setImageURLs] = useState([]);
+  const [userId, setUserId] = useState(null);
 
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
   };
 
   const { loading, data } = useQuery(QUERY_ME);
-  const userId = data?.me._id;
 
-  const fetchAnswers = async () => {
-    const openAIApiKey = "";
+  useEffect(() => {
+    if (!loading && data && data.me) {
+      setUserId(data.me._id);
+    }
+  }, [loading, data]);
 
-    const llm = new ChatOpenAI({ openAIApiKey });
-
-    const promptTemplate =
-      "You are in a AAC device. Give 10 1-5 word responses to the following topic or question to be used as a button to be pressed by an adult with developmental disabilities. do not start the response with a number separate each new response with a new line: {promptText}";
-
-    const responsePrompt = PromptTemplate.fromTemplate(promptTemplate);
-    const responseChain = responsePrompt.pipe(llm);
-    setResponses([]);
-
+  const fetchAnswersAndImages = async () => {
     try {
-      const result = await responseChain.invoke({
-        promptText: userInput,
-      });
-
-      setPromptText(userInput);
-      console.log("this is what I got back from chatgpt, ", result);
-      console.log("Question or Topic:", userInput);
-      console.log("Chat GPT Responses:", result.content);
-
-      const newResponses = result.content.split("\n");
-      console.log("This is the array we will map over, ", newResponses);
-      setResponses(newResponses);
-
+      // Make a request to your server
+      const response = await axios.post(
+        "http://localhost:3000/api/fetchAnswers",
+        { userInput }
+      );
+      console.log("here's the response from axios, ", response);
+      console.log(
+        "response.data.kwargs.content: ",
+        response.data.kwargs.content
+      );
+      const chatGPTResults = response.data.kwargs.content;
+      const chatGPTResultsArray = chatGPTResults.split("\n");
+      console.log("chatGPTResultsArray");
+      setResponses(chatGPTResultsArray);
+      console.log("here is the responses variable, ", responses);
       const newImageURLs = [];
-      // Loop through responses to fetch and save image URLs
-      newResponses.forEach((response, index) => {
-        const query = response;
+      for (let index = 0; index < responses.length; index++) {
+        const query = responses[index];
 
         const client = createClient(
           "THj5EwzyfSVYW1UvgByttwmIlcqXDvRS8AmbWtx587POTV86qPqdfd30"
         );
 
-        client.photos
-          .search({ query, per_page: 1 })
-          .then((data) => {
-            console.log("client.photos data", data);
-            const imageSrc =
-              data.photos.length > 0 ? data.photos[0].src.medium : null;
-            console.log(imageSrc);
-            newImageURLs[index] = imageSrc; // Save each image URL at the corresponding index
-            setImageURLs([...newImageURLs]); // Update the state with the new array of image URLs
-          })
-          .catch((error) => {
-            console.error("Error fetching Pexels data:", error);
-          });
-      });
+        try {
+          const data = await client.photos.search({ query, per_page: 1 });
+          const imageSrc =
+            data.photos.length > 0 ? data.photos[0].src.medium : null;
+          newImageURLs[index] = imageSrc;
+        } catch (error) {
+          console.error("Error fetching Pexels data:", error);
+        }
+      }
+
+      setImageURLs(newImageURLs);
     } catch (error) {
       console.error("Error fetching data from API:", error);
     }
@@ -85,7 +75,7 @@ export default function RecordButton() {
         placeholder="Enter a topic or question"
         className="input-field"
       />
-      <button className="fetch-button" onClick={fetchAnswers}>
+      <button className="fetch-button" onClick={fetchAnswersAndImages}>
         Fetch
       </button>
 
