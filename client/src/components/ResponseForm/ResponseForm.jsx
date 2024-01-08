351b083280cac50aef23081c3354fb57c6d54985
tmcmanus95 <tmcmanus95@gmail.com>
2024-01-08 01:08:22 -0600

    commented out pexels code'

--- client/src/components/ResponseForm/ResponseForm.jsx
+++ client/src/components/ResponseForm/ResponseForm.jsx
@@ -1,80 +1,80 @@
-// ResponseForm.jsx
-import { useState, useEffect } from "react";
-import { useMutation } from "@apollo/client";
-import { ADD_RESPONSE } from "../../../utils/mutations";
-import { createClient } from "pexels";
-import "./ResponseForm.scss";
-const ResponseForm = ({ index, responses }) => {
-  // const [response, setResponse] = useState("");
+// // ResponseForm.jsx
+// import { useState, useEffect } from "react";
+// import { useMutation } from "@apollo/client";
+// import { ADD_RESPONSE } from "../../../utils/mutations";
+// // import { createClient } from "pexels";
+// import "./ResponseForm.scss";
+// const ResponseForm = ({ index, responses }) => {
+//   // const [response, setResponse] = useState("");
 
-  const [addResponse, { error }] = useMutation(ADD_RESPONSE);
+//   const [addResponse, { error }] = useMutation(ADD_RESPONSE);
 
-  const speak = (text) => {
-    const utterance = new SpeechSynthesisUtterance(text);
-    window.speechSynthesis.speak(utterance);
-  };
+//   const speak = (text) => {
+//     const utterance = new SpeechSynthesisUtterance(text);
+//     window.speechSynthesis.speak(utterance);
+//   };
 
-  const handleResponseSubmit = async (event) => {
-    event.preventDefault();
+//   const handleResponseSubmit = async (event) => {
+//     event.preventDefault();
 
-    try {
-      const data = await addResponse({
-        variables: { topicId, responseText, imageURL: imageSrc },
-      });
+//     try {
+//       const data = await addResponse({
+//         variables: { topicId, responseText, imageURL: imageSrc },
+//       });
 
-      setResponse("");
-    } catch (err) {
-      console.error(err);
-    }
-  };
+//       setResponse("");
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
 
-  useEffect(() => {
-    if ("speechSynthesis" in window) {
-      // getting corresponding gif
-      responses.forEach((response, index) => {
-        const query = response;
+//   useEffect(() => {
+//     if ("speechSynthesis" in window) {
+//       // getting corresponding gif
+//       responses.forEach((response, index) => {
+//         const query = response;
 
-        const client = createClient(
-          "THj5EwzyfSVYW1UvgByttwmIlcqXDvRS8AmbWtx587POTV86qPqdfd30"
-        );
+//         const client = createClient(
+//           "THj5EwzyfSVYW1UvgByttwmIlcqXDvRS8AmbWtx587POTV86qPqdfd30"
+//         );
 
-        client.photos
-          .search({ query, per_page: 1 })
-          .then((data) => {
-            console.log("client.photos data", data);
-            const imageSrc =
-              data.photos.length > 0 ? data.photos[0].src.medium : null;
-            console.log(imageSrc);
-            const button = document.getElementById(`button-${index}`);
-            button.addEventListener("click", () => speak(response));
-            const gif = document.getElementById(`gif-${index}`);
-            gif.src = imageSrc;
-          })
-          .catch((error) => {
-            console.error("Error fetching Pexels data:", error);
-          });
-      });
-    }
-  }, [responses]);
+//         client.photos
+//           .search({ query, per_page: 1 })
+//           .then((data) => {
+//             console.log("client.photos data", data);
+//             const imageSrc =
+//               data.photos.length > 0 ? data.photos[0].src.medium : null;
+//             console.log(imageSrc);
+//             const button = document.getElementById(`button-${index}`);
+//             button.addEventListener("click", () => speak(response));
+//             const gif = document.getElementById(`gif-${index}`);
+//             gif.src = imageSrc;
+//           })
+//           .catch((error) => {
+//             console.error("Error fetching Pexels data:", error);
+//           });
+//       });
+//     }
+//   }, [responses]);
 
-  return (
-    <>
-      {responses.map((response, index) => (
-        <form className="responseButton" onSubmit={handleResponseSubmit}>
-          <div id={`button-${index}`} key={index}>
-            <div>
-              <p>{response}</p>
-              <span>
-                <button>save</button>
-              </span>
-            </div>
+//   return (
+//     <>
+//       {responses.map((response, index) => (
+//         <form className="responseButton" onSubmit={handleResponseSubmit}>
+//           <div id={`button-${index}`} key={index}>
+//             <div>
+//               <p>{response}</p>
+//               <span>
+//                 <button>save</button>
+//               </span>
+//             </div>
 
-            <img id={`gif-${index}`} alt={`gif-${index}`} />
-          </div>
-        </form>
-      ))}
-    </>
-  );
-};
+//             <img id={`gif-${index}`} alt={`gif-${index}`} />
+//           </div>
+//         </form>
+//       ))}
+//     </>
+//   );
+// };
 
-export default ResponseForm;
+// export default ResponseForm;
